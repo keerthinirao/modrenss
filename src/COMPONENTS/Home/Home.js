dc23ed2055a94ec0d6e460ac1bf9b8cf2d288b89
saicherry934 <cherry.workspace.mail@gmail.com>
2023-02-18 02:13:17 +0530

    first commit

---
+++ src/COMPONENTS/Home/Home.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Home.css'
+import SectionFive from './SectionFive'
+import SectionFour from './SectionFour'
+import SectionOne from './SectionOne'
+import SectionSix from './SectionSix'
+import SectionThree from './SectionThree'
+import SectionTwo from './SectionTwo'
+
+/*
+DESCRIPTION ON COMPONENT
+
+*/
+
+const Home = () => {
+  return (
+    <div>
+        <SectionOne></SectionOne>
+        <SectionTwo></SectionTwo>
+        <SectionThree></SectionThree>
+        <SectionFour></SectionFour>
+        <SectionFive></SectionFive>
+       
+    </div>
+  )
+}
+
+export default Home
